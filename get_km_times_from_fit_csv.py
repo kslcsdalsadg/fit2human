6593bdb2407c8cd080a396e91ece5bd028718c39
John Doe <31618496+kslcsdalsadg@users.noreply.github.com>
2025-02-05 16:50:53 +0100

    Add average time for each 5 Km interval

--- get_km_times_from_fit_csv.py
+++ get_km_times_from_fit_csv.py
@@ -21,24 +21,29 @@ def main():
             data = fit2human.main(source, 'json')
             if not data: print('Cannot load json data')
             else: data = json.loads(data)
-        else: 
-            with open(source) as json_file: data = json.load(json_file)  
+        else:
+            with open(source) as json_file: data = json.load(json_file)
         if data:
             print('Data from "%s"' % (source))
-            total_time, lap_times = 0, []
+            lap_times = []
             for lap in data['lap_mesgs']:
-                if lap['total_distance'] != 1000: 
+                if lap['total_distance'] != 1000:
                     break
-                else: 
-                    total_time += lap['total_elapsed_time']
+                else:
                     lap_times.append(lap['total_elapsed_time'])
             if len(lap_times) >= len(data['lap_mesgs']) - 1:
                 print()
-                for (i, lap_time) in enumerate(lap_times): print('KM %d:\t\t\t%s' % (i + 1, get_human_time(lap_time)))
-                print('\nAverage time per KM:\t%s' % (get_human_time(total_time / len(lap_times))))
-                print('\nTotal time\t\t%s\n' % (get_human_time(total_time)))
+                total_time = 0
+                for (i, lap_time) in enumerate(lap_times):
+                    print('KM %d:\t\t\t\t%s' % (i + 1, get_human_time(lap_time)))
+                    total_time = total_time + lap_time
+                print('\nAverage time per KM:\t\t%s\n' % (get_human_time(total_time / len(lap_times))))
+                total_time = 0
+                for (i, lap_time) in enumerate(lap_times):
+                    if (i > 0) and (i % 5 == 0): print('Average time per %02d KMs:\t%s' % (i, get_human_time(total_time)))
+                    total_time = total_time + lap_time
+                print('\nTotal time per %d KMs\t\t%s\n' % (len(lap_times), get_human_time(total_time)))
             else:
                 print('Lap times aren\'t valids')
-            
-if __name__ == "__main__": main()            
 
+if __name__ == "__main__": main()
